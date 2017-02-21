2fa4281ca8f9fccd499c4dabf3d5eb31e84e95c3
Francisco Morales <franmorales@gmail.com>
2017-02-21 18:24:15 +0100

    Routing

--- src/app/hero.service.js
+++ src/app/hero.service.js
@@ -8,8 +8,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('@angular/core');
 var mock_heroes_1 = require('./mock-heroes');
+var core_1 = require('@angular/core');
 var HeroService = (function () {
     function HeroService() {
     }
@@ -22,6 +22,10 @@ var HeroService = (function () {
             // Simulate server latency with 2 second delay
             setTimeout(function () { return resolve(_this.getHeroes()); }, 2000);
         });
+    };
+    HeroService.prototype.getHero = function (id) {
+        return this.getHeroes()
+            .then(function (heroes) { return heroes.find(function (hero) { return hero.id === id; }); });
     };
     HeroService = __decorate([
         core_1.Injectable(), 
